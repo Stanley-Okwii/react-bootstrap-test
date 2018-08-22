f7e22a06ad3f9c6fe357da73ed4374980c7ec9b9
Stanley-Okwii <stanleeparker12@gmail.com>
2018-08-22 08:14:55 +0200

    code refactor

--- src/index.js
+++ src/index.js
@@ -1,23 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Typeahead } from "react-bootstrap-typeahead";
-import { colourOptions } from "../src/Data/Data";
+import { colourOptions } from "./docs/data";
 
 import "./ui/styles.css";
 import "./ui/Typeahead.css";
 import "./ui/dropdown.less";
 
+
+// const Menu = props => (
+//   <div className="dropdown-menu">{props.children}</div>
+// );
+
+// const MenuList = props => (
+//   <div className="widget-dropdown-type-ahead-menu-list">{props.children}</div>
+// );
+
 function App() {
   return (
-    <div>
-      <h1 className="App">react-bootstrap-typeahead test</h1>
-      <div>
+    <div className="App">
+      <h1>react-bootstrap-typeahead test</h1>
+      <div className= "container">
         <Typeahead
           labelKey="label"
-          // clearButton={true}
+          clearButton={true}
           multiple={false}
           options={colourOptions}
           placeholder="Choose a color.."
+          // renderMenu = { Menu }
         />
       </div>
     </div>
